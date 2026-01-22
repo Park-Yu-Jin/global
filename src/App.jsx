d345ae74a5d16d93b40c45d80e707f464744b4a7
Park-Yu-jin <lits111@naver.com>
2026-01-22 09:19:32 +0900

    jscreate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import AA from './a.json'
 import './App.css'
-
+import {add} from './firebase'
 function App() {
-
+  const [count, setCount] = useState(0)  
   return (
     <>
+    <h1>{count}</h1>
+    <button onClick={()=>add(setCount)}>클릭</button>
       <h1>{AA.message}</h1>
       <h1>{AA.test}</h1>
-      <h2>{AA.data}</h2>
+      <h2>{AA.date}</h2>
     </>
   )
 }
